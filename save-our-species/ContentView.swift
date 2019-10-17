c565f7057d6f09bd19d567be669afab2ff544f17
Paul Yakovlev <yvpavel@gmail.com>
2019-10-16 23:32:04 -0700

    Initial Commit

---
+++ save-our-species/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  save-our-species
+//
+//  Created by Paul Yakovlev on 10/16/19.
+//  Copyright © 2019 MarineVP. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
