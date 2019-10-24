404b6608c59004b6a3335d8a03c0e2efa834c055
DSCommons <DSCommons@digscholar-06.library.ucsc.edu>
2019-10-23 21:01:19 -0700

    client view page

---
+++ save-our-species/ClientViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ClientViewController.swift
+//  save-our-species
+//
+//  Created by DSCommons on 10/23/19.
+//  Copyright © 2019 MarineVP. All rights reserved.
+//
+
+import UIKit
+
+class ClientViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBOutlet weak var ClientViewLabel: UILabel!
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
