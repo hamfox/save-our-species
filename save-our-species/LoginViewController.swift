90d28210569998507e0962a6fdcd5d402b811c67
DSCommons <DSCommons@digscholar-06.library.ucsc.edu>
2019-10-23 20:52:43 -0700

    home and login view not connected

---
+++ save-our-species/LoginViewController.swift
@@ -0,0 +1,31 @@
+//
+//  LoginViewController.swift
+//  save-our-species
+//
+//  Created by DSCommons on 10/23/19.
+//  Copyright © 2019 MarineVP. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBOutlet weak var LoginLabel: UILabel!
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
