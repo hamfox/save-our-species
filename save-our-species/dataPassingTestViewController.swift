d8480b26c1a13fe6d8ff9b65d37284634cd7f362
DSCommons <DSCommons@digscholar-05.library.ucsc.edu>
2019-11-07 11:59:32 -0800

    This is to send data forward to a new screen with segue

---
+++ save-our-species/dataPassingTestViewController.swift
@@ -0,0 +1,29 @@
+//
+//  dataPassingTestViewController.swift
+//  save-our-species
+//
+//  Created by DSCommons on 11/7/19.
+//  Copyright © 2019 MarineVP. All rights reserved.
+//
+
+import UIKit
+
+class dataPassingTestViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let longAsString = String(format:"%f", longValue!)
+        longLabel.text = longAsString
+        let latAsString = String(format:"%f", latValue!)
+        latLabel.text = latAsString
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBOutlet weak var longLabel: UILabel!
+    @IBOutlet weak var latLabel: UILabel!
+    
+    var longValue: Double?
+    var latValue: Double?
+    
+
+}
