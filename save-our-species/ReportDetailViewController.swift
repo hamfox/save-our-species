9039ace6ef00a8d24341fb181525916d1c29d901
Paul Yakovlev <yvpavel@gmail.com>
2019-11-07 15:11:41 -0800

    add detail view with navigation

---
+++ save-our-species/ReportDetailViewController.swift
@@ -0,0 +1,13 @@
+//
+//  ReportDetailViewController.swift
+//  save-our-species
+//
+//  Created by Paul Yakovlev on 11/7/19.
+//  Copyright © 2019 MarineVP. All rights reserved.
+//
+
+import UIKit
+
+class ReportDetailViewController: UIViewController {
+
+}
