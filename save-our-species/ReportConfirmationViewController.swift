d446c6023b5e53cccadb7c561e9c0b7a9a7f4e57
DSCommons <DSCommons@digscholar-05.library.ucsc.edu>
2019-11-18 12:44:26 -0800

    Confirmation screen now shows user description as well as lat+long values

--- save-our-species/ReportConfirmationViewController.swift
+++ save-our-species/ReportConfirmationViewController.swift
@@ -13,10 +13,22 @@ class ReportConfirmationViewController: UIViewController {
     var latValue: Double? = 0
     var descriptionText: String?
 
+    @IBOutlet weak var descriptionField: UILabel!
+    
+    @IBOutlet weak var latitudeField: UILabel!
+    
+    @IBOutlet weak var longitudeField: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         let report = Report(description: descriptionText!, latitude: latValue!, longitude: longValue!)
+        descriptionField.text = descriptionText
+        let latAsString:String = String(format:"%f", latValue!)
+        let longAsString:String = String(format:"%f", longValue!)
+        latitudeField.text = latAsString
+        longitudeField.text = longAsString
         
-        print(report?.description,report?.latitude,report?.longitude)
+        
+        //print(report?.description,report?.latitude,report?.longitude)
     }
 }
