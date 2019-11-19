4ebcb7fb8acdd551ee72500a1beb136421ece5e3
Paul Yakovlev <yvpavel@gmail.com>
2019-11-19 12:27:34 -0800

    convert coordinates to human readable address

--- save-our-species/ReportDetailViewController.swift
+++ save-our-species/ReportDetailViewController.swift
@@ -7,6 +7,8 @@
 //
 
 import UIKit
+import MapKit
+import CoreLocation
 
 class ReportDetailViewController: UIViewController {
 
@@ -16,11 +18,26 @@ class ReportDetailViewController: UIViewController {
     @IBOutlet weak var LocationFieldLabel: UILabel!
     
     var detailReport = Report(description: "", latitude: 0.0, longitude: 0.0)
-    var message : String?
+
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        getReadableLocation()
         DescriptionFieldLabel.text = detailReport?.description
         print("DETAILVIEW: Received report description:",detailReport?.description)
     }
+    
+    func getReadableLocation() {
+        let address = CLGeocoder.init()
+        address.reverseGeocodeLocation(CLLocation.init(latitude: detailReport!.latitude, longitude:detailReport!.longitude)) { (placeMarks, error) in
+            if error == nil{
+                    if let placeMark = placeMarks?[0]{
+                        print(placeMark)
+                        print("THIS THE PLACE:",placeMark.name)
+                        
+                    }
+            }
+        }
+    }
+    
 }
