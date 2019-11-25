92d9cb06bab8a59b7cc468911f6463a764ba32f4
Paul Yakovlev <yvpavel@gmail.com>
2019-11-24 23:31:38 -0800

    send report to firebase

--- save-our-species/ReportConfirmationViewController.swift
+++ save-our-species/ReportConfirmationViewController.swift
@@ -9,22 +9,26 @@
 import UIKit
 
 class ReportConfirmationViewController: UIViewController {
+    let reportService = ReportService()
+
     var longValue: Double? = 0
     var latValue: Double? = 0
     var descriptionText: String? = ""
     var reportTime: String? = ""
+    var report = Report(description: "", latitude: 0.0, longitude: 0.0, reportTime: "")
 
     @IBOutlet weak var descriptionField: UILabel!
     @IBOutlet weak var latitudeField: UILabel!
     @IBOutlet weak var longitudeField: UILabel!
     
     @IBAction func sendReportButtonTapped(_ sender: UIButton) {
+        addReport(report: report!)
     }
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        let report = Report(description: descriptionText!, latitude: latValue!, longitude: longValue!, reportTime: reportTime!)
+        report = Report(description: descriptionText!, latitude: latValue!, longitude: longValue!, reportTime: reportTime!)
         descriptionField.text = descriptionText
         let latAsString:String = String(format:"%f", latValue!)
         let longAsString:String = String(format:"%f", longValue!)
@@ -33,5 +37,12 @@ class ReportConfirmationViewController: UIViewController {
         
         print(descriptionText)
         print("REPORTED AT: ",reportTime)
+    }
+    
+    func addReport(report: Report) {
+        reportService.addToList(report: report, completion: { (status) in
+        if status {
+            print("Status", status)
+        }})
     }
 }
